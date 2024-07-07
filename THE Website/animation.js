cc6d3e4edcdc89eafadb91709f230a54b0068826
Jannick Klose <ja.l.klose@gmail.com>
2024-07-07 16:06:24 +0200

    RAGE

---
+++ THE Website/animation.js
@@ -0,0 +1,25 @@
+"use strict";
+let canvas = document.querySelector("canvas");
+let crcr2 = canvas.getContext("2d");
+//crcr2.fillStyle = "yellow";
+//crcr2.fillRect(0, 0, crcr2.canvas.width, crcr2.canvas.height);
+//let Imageee: HTMLImageElement = document.querySelector("img")!;
+//Imageee.addEventListener("mouseover", mouseHover);
+/*let wawawu: Path2D = new Path2D;
+wawawu.rect(580, 380, 930, 580);
+crcr2.fillStyle = "indianred";
+crcr2.fill(wawawu);
+*/
+crcr2.beginPath();
+crcr2.ellipse(40, 40, 40, 30, 0, 0, 6);
+crcr2.fillStyle = "red";
+crcr2.fill();
+crcr2.closePath();
+function mouseHover(_event) {
+    let x = _event.offsetX;
+    let y = _event.offsetY;
+    //crcr2.isPointInPath
+    console.log("yeeeeeeeeeeeeet");
+    //Imageee.style.width = 120 + "%";
+}
+//document.body.addEventListener("mouseover", mouseHover);
